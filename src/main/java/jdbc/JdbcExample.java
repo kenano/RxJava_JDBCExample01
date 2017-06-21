c80c615ce924ad9cd1640d60ab4fa6cdddc4f5ae
Kenan Ozdamar <kenan.ozdamar@turner.com>
2017-06-21 14:40:19 -0400

    Basic JDBC interaction using Derby and JavaRX

--- src/main/java/jdbc/JdbcExample.java
+++ src/main/java/jdbc/JdbcExample.java
@@ -1,10 +1,44 @@
 package jdbc;
 
+import rx.Observable;
+import rx.util.functions.Action1;
+import rx.util.functions.Func0;
+import rx.util.functions.Func1;
+import utils.ThreadUtils;
+
 /**
  * Created by kenanozdamar on 6/19/17.
  */
 public class JdbcExample {
     public static void main(String[] arg) {
+
+        // Create a simple error handler that will emit the stack trace for a given exception
+        Action1<Throwable> simpleErrorHandler = (throwable) ->{
+            throwable.printStackTrace();
+        };
+
+        // Initialize the test Derby database...this also resets its contents
         TestDatabase.init();
+
+        // Resource Factory function to create a TestDatabase connection and wrap it in a ConnectionSubscription
+        Func0<ConnectionSubscription> resourceFactory = () -> {
+            return new ConnectionSubscription(TestDatabase.createConnection());
+        };
+
+        // Observable Factory function to create the resultset that we want.
+        Func1<ConnectionSubscription, Observable<String>> greekAlphabetList = (connectionSubscription) -> {
+            return TestDatabase.selectGreekAlphabet(connectionSubscription);
+        };
+
+        Observable<String> t = Observable.using(resourceFactory, greekAlphabetList);
+        t.subscribe(
+                (letter) -> {
+                    System.out.println(ThreadUtils.currentThreadName() + " - " + letter);
+                },
+                simpleErrorHandler,
+                () -> {
+                    System.out.println( "onCompleted" );
+                });
+
     }
 }
---
+++ src/main/java/utils/ThreadUtils.java
@@ -0,0 +1,27 @@
+package utils;
+
+/**
+ * Created by kenanozdamar on 6/20/17.
+ */
+public class ThreadUtils {
+    public static void sleep( long ms ) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException ex) {
+            // suppress
+        }
+    }
+
+    public static void wait( Object monitor ) {
+        try {
+            // Assumes that the monitor is already "synchronized"
+            monitor.wait();
+        } catch (InterruptedException ex) {
+            // suppress
+        }
+    }
+
+    public static String currentThreadName() {
+        return Thread.currentThread().getName();
+    }
+}
